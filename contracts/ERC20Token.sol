07939768d26c8f33dd9362ef7019895af61a0fdf
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2019-03-24 14:33:51 -0300

    Udemy Course: Code an ERC-20 Token on the Ethereum blockchain sell it with a crowd sale (ICO) website
      - 4. Transferring Tokens
        building the transfer function
        building the Transfer event
        building the unit test

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -8,9 +8,22 @@ contract ERC20Token {
 
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender]   = _initialSupply;
         totalSupply             = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, 'Invalid value to transfer');
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 
 }
--- test/ERC20TokenTest.js
+++ test/ERC20TokenTest.js
@@ -2,7 +2,8 @@ var ERC20Token = artifacts.require("./ERC20Token.sol");
 
 contract('ERC20Token', function(accounts) {
   var tokenInstance,
-      adminAccount = accounts[0];
+      adminAccount = accounts[0],
+      toAccount    = accounts[1];
   
 
   it('initializes the contract attributes', function() {
@@ -29,6 +30,32 @@ contract('ERC20Token', function(accounts) {
       return tokenInstance.balanceOf(adminAccount);
     }).then(function(adminBalance) {
       assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin balance');
+    });
+  });
+
+  it('transfers token ownership', function() {
+    return ERC20Token.deployed().then(function(instance) {
+      tokenInstance = instance;
+      // Test 'require' statement first by transferring something larger the sender's balance
+      return tokenInstance.transfer.call(toAccount, 9000000);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+      return tokenInstance.transfer.call(toAccount, 250000, { from: adminAccount});
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true success');
+      return tokenInstance.transfer(toAccount, 250000, { from: adminAccount});
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, adminAccount, 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      return tokenInstance.balanceOf(toAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+      return tokenInstance.balanceOf(adminAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
     });
   });
   
