ed06e720a65703af3ced0f1bfcca8b123e402e65
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2019-03-29 07:34:20 -0300

    Udemy Course: Code an ERC-20 Token on the Ethereum blockchain sell it with a crowd sale (ICO) website   - Crowd Sale Smart Contract   - 9. Purchasing ERC-20 Tokens     define attributes: tokensSold     building the functions: buyTokens, multiply     building the event Sell     building the unit test

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -12,7 +12,7 @@ contract ERC20Token {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    constructor (uint256 _initialSupply) public {
+    constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender]   = _initialSupply;
         totalSupply             = _initialSupply;
     }
