c63aab9ad0a09ca7d221f18476b1b680f259ad2e
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2019-03-31 17:56:26 -0300

    Udemy Course: Code an ERC-20 Token on the Ethereum blockchain sell it with a crowd sale (ICO) website   - Crowd Sale Website   - 13. Purchasing ERC-20 Tokens     building function in application app.js     completing render function     building buy tokens function     building the event function to listen sell event     including provision script to token sale contract in readme.md file

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Code an ERC-20 Token on the Ethereum blockchain sell it with a crowd sale (ICO) 
   - lite-server v2.4.0 (https://github.com/johnpapa/lite-server)
   - Bootstrap v4.3.1 (https://getbootstrap.com/)
 
+### Run app:
+  npm run dev
+  http://localhost:3000/
+
 ### Links:
 
   - Course: 
@@ -17,3 +21,16 @@ Code an ERC-20 Token on the Ethereum blockchain sell it with a crowd sale (ICO) 
     https://eips.ethereum.org/EIPS/eip-20
     https://en.wikipedia.org/wiki/ERC-20
     https://theethereum.wiki/w/index.php/ERC20_Token_Standard
+
+
+
+### Provision tokens:
+  - Provision 75% of all tokens to the token sale
+  > truffle console
+
+  ERC20TokenSale.deployed().then(function (i) { tokenSale = i; })
+  ERC20Token.deployed().then(function (i) { token = i; })
+  tokensAvailable = 750000
+  web3.eth.getAccounts().then(function (acc) { admin = acc[0]; })
+  token.transfer(tokenSale.address, tokensAvailable, { from: admin })
+  token.balanceOf(tokenSale.address).then(function (b) { balance = b; })
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
     web3Provider: null,
     contracts: {},
     account: "0x0",
+    loading: false,
+    tokenPrice: 1000000000000000,
+    tokensSold: 0,
+    tokensAvailable: 750000,
 
     init: function () {
         console.log("App initialized...");
@@ -36,19 +40,100 @@ App = {
                     console.log("ERC20 Token Address: ", erc20Token.address);
                 });
             });
+
+            App.listenForEvents();
             return App.render();
         })
     },
 
+    // Listen for events emitted from the contract
+    listenForEvents: function () {
+        App.contracts.ERC20TokenSale.deployed().then(function (instance) {
+            instance.Sell({}, {
+                fromBlock: 0,
+                toBlock: 'latest',
+            }).watch(function (error, event) {
+                console.log("event triggered", event);
+                App.render();
+            })
+        })
+    },
+
     render: function () {
+        if (App.loading) {
+            return;
+        }
+        App.loading = true;
+
+        var loader = $('#loader');
+        var content = $('#content');
+
+        loader.show();
+        content.hide();
+
         // Load account data
         web3.eth.getCoinbase(function (err, account) {
+            // if return null where using MetaMask plugin, observe the Setting > Security > Privacy mode must off
+            // https://github.com/MetaMask/metamask-extension/issues/5716
+            console.log('get coinbase account: ', account);
             if (err === null) {
                 App.account = account;
-                $('#accountAddress').html("Your Account: " + account);
+                $("#accountAddress").html("Your Account: " + account);
             }
         });
+
+        //Load token sale contract
+        App.contracts.ERC20TokenSale.deployed().then(function (instance) {
+            erc20TokenSaleInstance = instance;
+            return erc20TokenSaleInstance.tokenPrice();
+        }).then(function (tokenPrice) {
+            App.tokenPrice = tokenPrice;
+            $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+            return erc20TokenSaleInstance.tokensSold();
+        }).then(function (tokensSold) {
+            App.tokensSold = tokensSold.toNumber();
+            $('.tokens-sold').html(App.tokensSold);
+            $('.tokens-available').html(App.tokensAvailable);
+
+            var progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+            $('#progress').css('width', progressPercent + '%');
+
+            //Load token contract
+            App.contracts.ERC20Token.deployed().then(function (instance) {
+                erc20TokenInstance = instance;
+                return erc20TokenInstance.balanceOf(App.account);
+            }).then(function (balanceOf) {
+                console.log('balanceOf: ', balanceOf);
+                $('.dapp-balance').html(balanceOf.toNumber());
+
+                App.loading = false;
+                loader.hide();
+                content.show();
+            });
+        });
+    },
+
+    buyTokens: function () {
+        console.log("call buyTokens...");
+        $('#loader').show();
+        $('#content').hide();
+
+        var numberOfTokens = $('#numberOfTokens').val();
+        console.log("numberOfTokens buy: ", numberOfTokens);
+
+        App.contracts.ERC20TokenSale.deployed().then(function (instance) {
+            return instance.buyTokens(numberOfTokens, {
+                from: App.account,
+                value: numberOfTokens * App.tokenPrice,
+                gas: 500000 // Gas limit
+            });
+        }).then(function (result) {
+            console.log("Tokens bought...")
+            $('form').trigger('reset') // reset number of tokens in form
+            // Wait for Sell event
+        });
     }
+
 }
 
 $(function () {
